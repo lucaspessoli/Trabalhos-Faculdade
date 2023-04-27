ae4de91966e2bddf826e47aa1e7b28d0ba19c936
Lucas Pessoli <pessolilucas@gmail.com>
2023-04-26 22:02:20 -0300

    Atualizei a MAIN

--- Atividades JAVA/ExercicioJava 26-04-02023/MegaExercicio.java
+++ Atividades JAVA/ExercicioJava 26-04-02023/MegaExercicio.java
@@ -11,7 +11,45 @@ package com.mycompany.megaexercicio;
 public class MegaExercicio {
 
     public static void main(String[] args) {
+        Ingresso in = new Ingresso(25);
         IngressoVIP i = new IngressoVIP(250);
+        
+        System.out.println(in.toString());
         System.out.println(i.toString());
+        
+        Empregado e = new Empregado("jorjao", 2500);
+        Gerente g = new Gerente("Departamentao","Gerentao", 900);
+        Vendedor v = new Vendedor("pelicano", 2000, 10);
+        
+        System.out.println("======================================================================");
+
+        
+        System.out.println(e.toString());
+        System.out.println("Total com comissao: "+ v.CalcularComissao(200));
+        System.out.println(g.toString());
+        
+        ContaBancaria cb = new ContaBancaria("joao", 2, 2500);
+        ContaPoupanca cc = new ContaPoupanca(cb.getCliente(), cb.getNum_conta(), cb.getSaldo(), 44);
+        ContaEspecial ce = new ContaEspecial(cb.getCliente(), cb.getNum_conta(), cb.getSaldo(),-300);
+        
+        ce.Sacar(2800);
+        cc.CalcularNovoSaldo(2500);
+        cb.SacarCB(300);
+        
+        System.out.println("======================================================================");
+
+        
+        Animal an = new Animal("leao", 2, 3, "vermelho", "terra", 25);
+        Peixe pe = new Peixe(an.getNome(), an.getComprimento(), an.getPatas(), an.getCor(), an.getAmbiente(), an.getVelocidade(), "legal");
+        Mamifero ma = new Mamifero(an.getNome(), an.getComprimento(), an.getPatas(), an.getCor(), an.getAmbiente(), an.getVelocidade(), "feijao");
+        
+        System.out.println("======================================================================");
+        
+        an.Dados();
+        pe.Caracteristica();
+        pe.Dados();
+        ma.Alimento();
+        ma.Dados();
+        
     }
 }
