85fbf7e65eec68f9f47a9c3f51804645da54964a
Lucas Pessoli <pessolilucas@gmail.com>
2023-05-17 19:45:14 -0300

    IMPLEMENT enumeracao atividade

--- Atividades JAVA/Trabalho Pilares/TrabalhoPilares6/src/main/java/com/mycompany/trabalhopilares6/TrabalhoPilares6.java
+++ Atividades JAVA/Trabalho Pilares/TrabalhoPilares6/src/main/java/com/mycompany/trabalhopilares6/TrabalhoPilares6.java
@@ -12,7 +12,7 @@ public class TrabalhoPilares6 {
 
     public static void main(String[] args) {
         
-        //Crie uma classe chamda pessoa com atributos privados, como um nome e idade. U\tilize o encapsulamento para permitir o acesso a esses atributos por meio de getters & setters
+        //6 - Crie uma classe chamda pessoa com atributos privados, como um nome e idade. U\tilize o encapsulamento para permitir o acesso a esses atributos por meio de getters & setters
         
         Pessoa pessoa = new Pessoa();
         
