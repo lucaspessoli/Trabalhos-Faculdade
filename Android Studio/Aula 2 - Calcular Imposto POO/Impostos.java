2686b5c0a019c70b73ab56c62a9016441b9c8931
Lucas Pessoli <pessolilucas@gmail.com>
2023-08-22 22:13:43 -0300

    ADD Aula 2 Calculo de impostos usando POO

---
+++ Android Studio/Aula 2 - Calcular Imposto POO/Impostos.java
@@ -0,0 +1,14 @@
+package com.example.exercicio2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Impostos extends AppCompatActivity {
+    public double rendaBrutaAnual;
+    public double baseCalculo;
+    public double imposto;
+
+}
